95ea00cca831698f74b4f32a9f4c9116f157fd80
Christina Lin <Christinalin006@gmail.com>
2026-08-11 00:06:17 -0400

    removed app.py

--- strength_acknowledgement/app.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # It will automatically find .env in the same folder now!
-api_key = os.getenv("OPENAI_API_KEY")
-print("Loaded successfully!" if api_key else "Key not found.")
